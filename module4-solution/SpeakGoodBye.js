a20901fec4e7d93f02ce89f597e0e4beb25ec1ce
ramana-mv <72978299+ramana-mv@users.noreply.github.com>
2021-02-07 12:30:37 +0530

    Delete module4-solution directory

--- module4-solution/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-  var byeSpeaker={};
-  byeSpeaker.speak=function (name) {
-    var speakWord = "Good Bye";
-    console.log(speakWord + " " + name);
-  }
-  window.byeSpeaker=byeSpeaker;
-})(window);
--- module4-solution/SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  var helloSpeaker={};
-  helloSpeaker.speak=function (name) {
-    helloSpeaker.name=name;
-    var speakWord = "Hello";
-    console.log(speakWord + " " + helloSpeaker.name);
-  }
-  window.helloSpeaker=helloSpeaker;
-})(window);
